ee3814f5e6784704163d8527e8394f665b4a8ecf
Marco Castanho <marco.castanho@toptal.com>
2024-02-26 10:42:20 +0000

    cache updateQuery

--- client/src/lib/graphql/hooks.js
+++ client/src/lib/graphql/hooks.js
@@ -1,12 +1,22 @@
-import { useMutation, useQuery } from '@apollo/client';
-import { addMessageMutation, messagesQuery } from './queries';
+import { useMutation, useQuery } from "@apollo/client";
+import { addMessageMutation, messagesQuery } from "./queries";
 
 export function useAddMessage() {
   const [mutate] = useMutation(addMessageMutation);
 
   const addMessage = async (text) => {
-    const { data: { message } } = await mutate({
+    const {
+      data: { message },
+    } = await mutate({
       variables: { text },
+      // this updates the cache
+      // this way the message a user just sent is added to their cache, becoming immediately visible to them
+      update: (cache, { data }) => {
+        const newMessage = data.message;
+        // this means: when the messagesQuery is resolved,
+        // update the cache data using the following function
+        cache.updateQuery({ query: messagesQuery }, (oldData) => ({ messages: [...oldData.messages, newMessage] }));
+      },
     });
     return message;
   };
